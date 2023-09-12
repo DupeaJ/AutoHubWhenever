31c2c4c75bf6618bbc411bb4778fa55cf86c1e7e
unknown <jacobdupea@gmail.com>
2023-09-11 17:30:10 -0700

    connected mysql to jawsdb

--- config/database.js
+++ config/database.js
@@ -1,11 +1,8 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+const db = mysql.createConnection(
+    process.env.JAWSDB_URL
+)
 
 db.connect((err) => {
     if (err) {

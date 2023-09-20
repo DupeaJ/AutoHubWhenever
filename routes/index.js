6b9402ccc49726d77d4d6e32b7f08968fab06fea
unknown <jacobdupea@gmail.com>
2023-09-20 15:05:48 -0700

    centered register page added logout buttons

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const checkAuthenticated = require('../middleware/checkAuthenticated');
 
 router.get("/", checkAuthenticated, (req, res) => {
     console.log(req.user);
-    res.render("index.ejs", { name: req.user ? req.user.name : "guest" });
+    res.render("index.ejs", {
+        name: req.user ? req.user.name : "guest",
+        carImage: req.user && req.user.carImage ? req.user.carImage : null,
+    });
 });
+
 
 module.exports = router;
